44511fddc1b493f20699e07c84aa0d039bf1f7ef
Ksenia <knazarova9@yandex.ru>
2016-10-21 14:24:15 +0300

    doodle view + added permission to write

---
+++ Doodlz/app/src/main/java/net/ucoz/ksen/doodlz/DoodleView.java
@@ -0,0 +1,15 @@
+package net.ucoz.ksen.doodlz;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Kseniya on 21.10.2016.
+ */
+
+public class DoodleView extends View {
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
