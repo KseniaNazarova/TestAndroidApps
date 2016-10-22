28b864fbf4fae335a240823a15bbde9f217e16e2
Ksenia <knazarova9@yandex.ru>
2016-10-22 20:30:52 +0300

    listeners

--- Doodlz/app/src/main/java/net/ucoz/ksen/doodlz/MainActivityFragment.java
+++ Doodlz/app/src/main/java/net/ucoz/ksen/doodlz/MainActivityFragment.java
@@ -1,8 +1,16 @@
 package net.ucoz.ksen.doodlz;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,12 +19,114 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private DoodleView doodleView;
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+    private boolean dialogOnScreen = false;
+
+    private static final int ACCELERATION_THRESHOLD = 100000;
+    private static final int SAVE_IMAGE_PERMISSION_REQUEST_CODE = 1;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        setHasOptionsMenu(true);
+        doodleView = (DoodleView) view.findViewById(R.id.doodleView);
+
+        acceleration = 0.00f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.doodle_fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.color:
+                ColorDialogFragment colorDialog = new ColorDialogFragment();
+                colorDialog.show(getFragmentManager(), "color dialog");
+                return true;
+            case R.id.line_width:
+                LineWidthDialogFragment widthDialog = new LineWidthDialogFragment();
+                widthDialog.show(getFragmentManager(), "line width dialog");
+                return true;
+            case R.id.delete_drawing:
+                confirmErase();
+                return true;
+            case R.id.save:
+                saveImage();
+                return true;
+            case R.id.print:
+                doodleView.printImage();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableAccelerometerListening();
+    }
+
+    private void enableAccelerometerListening() {
+        SensorManager sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableAccelerometerListening();
+    }
+
+    private void disableAccelerometerListening(){
+        SensorManager sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    private final SensorEventListener sensorEventListener =
+            new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    if (!dialogOnScreen){
+                        float x = event.values[0];
+                        float y = event.values[1];
+                        float z = event.values[2];
+
+                        lastAcceleration = currentAcceleration;
+                        currentAcceleration = x * x + y * y + z * z;
+                        acceleration = currentAcceleration * (currentAcceleration - lastAcceleration);
+
+                        if (acceleration > ACCELERATION_THRESHOLD)
+                            confirmErase();
+                    }
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+            };
+
+    private void confirmErase() {
+        EraseImageDialogFragment fragment = new EraseImageDialogFragment();
+        fragment.show(getFragmentManager(), "erase dialog");
+    }
+
+    private void saveImage(){
+
     }
 }
+
